0d607d8180496d13395388864792397022394842
Liu Yuanjing <lauyuanjing@163.com>
2019-02-28 15:40:06 +0800

    mkdir py

---
+++ tt_jks.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2019/2/28 15:36
+# @Author  : yuanjing liu
+# @Email   : lauyuanjing@163.com
+# @File    : tt_jks.py
+# @Software: PyCharm
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/test_jenkins/")
+def avs_info():
+    return "hello jenkins"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
